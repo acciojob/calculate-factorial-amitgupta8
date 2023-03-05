e1e415844b830959eff00fd557a11e2f8da126f9
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-05 16:47:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let num = prompt("Enter a number:");
+let factorial = 1;
+
+for(let i = 1; i <= num; i++){
+  factorial *= i;
+}
+
+alert("The factorial of " + num + " is " + factorial);
+
